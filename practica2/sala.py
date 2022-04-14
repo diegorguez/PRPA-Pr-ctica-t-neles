856c37a35a92ddc1466df39d2e24fc173f63c9fe
pablsa09 <98520918+pablsa09@users.noreply.github.com>
2022-04-14 17:19:01 +0200

    Update sala.py
    Corrijo algunas cosas y hago la clase Car1

--- practica2/sala.py
+++ practica2/sala.py
@@ -2,6 +2,7 @@ from multiprocessing.connection import Listener
 from multiprocessing import Process, Manager, Value, Lock
 import traceback
 import sys
+import random as rd
 
 HEIGHT=400
 WIDTH=600
@@ -15,10 +16,13 @@ class Player():
     self.side=side
     if side==PLAYER_ONE
       self.pos=[WIDHT/4,0]
+      #self.pos=[WIDHT/4,HEIGHT]
     if side==PLAYER_TWO
       self.pos=[WIDTH/2,0]
+      #self.pos=[WIDTH/2,HEIGHT]
     if side==PLAYER_THREE
       self.pos=[3*WIDTH/4,0]
+      #self.pos=[3*WIDTH/4,HEIGHT]
   
   def get_pos(self):
     return self.pos
@@ -27,20 +31,36 @@ class Player():
     return self.side
  
   def moveDown(self):
-    self.pos[Y]+=MOVEMENT
-    if self.pos[Y]>HEIGHT:
-      self.pos[Y]=HEIGHT
+    self.pos[1]+=MOVEMENT
+    if self.pos[1]>HEIGHT:
+      self.pos[1]=HEIGHT
   
   def moveUP(self):
-    self.pos[Y]+=MOVEMENT
-    if self.pos[Y]<0:
-      self.pos[Y]=0
+    self.pos[1]+=MOVEMENT
+    #self.pos[1]-=MOVEMENT
+    if self.pos[1]<0:
+      self.pos[1]=0
       
   def __str__(self):
     return f"P<{SIDESSTR[self.side]},{self.pos}>"
       
   
 class Car1():
+  def __init__(self,index):
+    self.x = rd.randint(-1000,-1) #Colocamos los cohces1 a la izquierda de form aleatoria
+    self.y = rd.randint(0,HEIGHT-10)
+    self.pos = [self.x , self.y]
+    self.vel = random.randint(8,20) 
+    
+    def get_pos(self):
+        return self.pos         
+        
+    def update(self):        
+        self.pos[Y] = self.pos[Y] 
+        self.pos[X] += self.vel  
+     
+    def __str__(self):
+        return f"B<{self.pos}>"
   
 class Car2():
   
