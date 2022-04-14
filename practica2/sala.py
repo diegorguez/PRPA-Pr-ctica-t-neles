bb98e970c5c85d73481b49d007c497cd961ac349
diegorguez <98813907+diegorguez@users.noreply.github.com>
2022-04-14 15:09:34 +0200

    Update sala.py

--- practica2/sala.py
+++ practica2/sala.py
@@ -11,3 +11,37 @@ PLAYER_THREE=2
 FPS=30
 
 class Player():
+  
+class Car1():
+  
+class Car2():
+  
+class Car3():
+  
+class Game():
+  
+def player(side,conn,game):
+  try:
+    print(f"starting player {SIDESSTR[side]}:{game.get_info()}")
+    conn.send_((side,game.get:info()))
+    while game.is_running():
+      command=""
+      while command != "next":
+        command=conn.recv()
+        if command=="up":
+          game.moveUp(side)
+        elif command=="down":
+          game.moveDown(side)
+  except:
+    traceback.print_exc()
+    conn.close()
+  finally:
+    print(f"Game ended {game}")
+    
+def main(ip_adrress):
+  
+  
+if __name__=='__main__':
+  
+  
+         
